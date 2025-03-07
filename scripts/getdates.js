78d8824170ff83cf8d01a3fa84427882bfddc921
clane83 <clane83@yahoo.com>
2025-03-06 20:19:43 -0800

    updated java script for last modify date

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,8 +3,4 @@ let year = today.getFullYear();
 document.getElementById("currentYear").innerHTML = year;
 
 
-// const lastModified = new Date();
-let day = today.getDate();
-let month = today.getMonth() + 1;
-let modify = "Last Modification: " + month + "/" + day + "/" + year
-document.getElementById("lastModified").innerHTML = modify;
+document.getElementById("lastModified").textContent = document.lastModified;
