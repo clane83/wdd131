cbabe505c5daba8f2812e1937c8467664a6eb722
clane83 <clane83@yahoo.com>
2025-03-07 08:44:43 -0800

    implement lazy load javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", function() {
+    let script = document.createElement("script");
+    script.src = "heavy-script.js";
+    document.body.appendChild(script);
+});
+
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
      
 document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
