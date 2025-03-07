ba9a0e9427a4899fb38c1de422ee750bd5c9ffae
clane83 <clane83@yahoo.com>
2025-03-07 08:47:39 -0800

    removed CSP and lazy load javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,4 @@
-window.addEventListener("load", function() {
-    let script = document.createElement("script");
-    script.src = "heavy-script.js";
-    document.body.appendChild(script);
-});
+
 
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();
      
