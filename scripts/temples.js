7d127d4c9fbe5107849e5cf5ccab4139295f1a7d
clane83 <clane83@yahoo.com>
2025-03-11 16:52:39 -0700

    hamburger menu working

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,10 +1,13 @@
-document.getElementById("currentYear").innerHTML = new Date().getFullYear();     
-document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
+document.addEventListener("DOMContentLoaded", () => {
 
-// const hamButton = document.querySelector("#menu");
-// const navigation = document.querySelector(".navigation");
+    document.getElementById("currentYear").innerHTML = new Date().getFullYear();     
+    document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
 
-// hamButton.addEventListener("click", () => {
-// 	navigation.classList.toggle("open");
-// 	hamButton.classList.toggle("open");
-// });
+    const hamButton = document.querySelector("#menu");
+    const navigation = document.querySelector(".navigation");
+
+    hamButton.addEventListener("click", () => {
+	    navigation.classList.toggle("open");
+	    hamButton.classList.toggle("open");
+    });
+});
