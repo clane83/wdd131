5d930b1d232fd9d0b9d2761b38f9ba8ad5485a39
clane83 <clane83@yahoo.com>
2025-03-11 16:20:26 -0700

    menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,2 +1,10 @@
 document.getElementById("currentYear").innerHTML = new Date().getFullYear();     
 document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
+
+// const hamButton = document.querySelector("#menu");
+// const navigation = document.querySelector(".navigation");
+
+// hamButton.addEventListener("click", () => {
+// 	navigation.classList.toggle("open");
+// 	hamButton.classList.toggle("open");
+// });
