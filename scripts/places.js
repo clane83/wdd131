878f633797dc86d1d2b4552ccbffe1e80c40b86d
clane83 <clane83@yahoo.com>
2025-03-21 17:14:06 -0700

    places.css, places.html and places.js updated for small screen

---
+++ scripts/places.js
@@ -0,0 +1,5 @@
+
+
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+     
+document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
