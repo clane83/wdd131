1911f7e9c8f820a9d9ad5b2f0ea0c69983dc5304
clane83 <clane83@yahoo.com>
2025-04-05 09:22:42 -0700

    nav is working between home and movie reviews

--- project/scripts/moviereview.js
+++ project/scripts/moviereview.js
@@ -2,13 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("currentYear").innerHTML = new Date().getFullYear();     
     document.getElementById("lastModified").textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
 
-    // const hamButton = document.querySelector("#menu");
-    // const navigation = document.querySelector(".navigation");
+    const hamButton = document.querySelector("#menu");
+    const navigation = document.querySelector(".navigation");
 
-    // hamButton.addEventListener("click", () => {
-    //     navigation.classList.toggle("open");
-    //     hamButton.classList.toggle("open");
-    // });
+    hamButton.addEventListener("click", () => {
+        navigation.classList.toggle("open");
+        hamButton.classList.toggle("open");
+    });
 
     const movies = [
         {
